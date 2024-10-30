27732983d6eb1a0bcb1eb57377e779698e775229
amr89-dev <amr89.dev@gmail.com>
2024-10-30 14:14:10 -0500

    Enhance CORS configuration to restrict origins for better security in app.js

--- src/app.js
+++ src/app.js
@@ -3,8 +3,18 @@ const apiRouter = require("./router/index.js");
 const cors = require("cors");
 
 const createServer = () => {
+  const ACCEPTED_ORIGINS = ["https://busca-precios-cedar.vercel.app"];
+  const options = {
+    origin: (origin, callback) => {
+      if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+  };
   const server = express();
-  server.use(cors());
+  server.use(cors(options));
   server.use(express.json());
   apiRouter(server);
 
