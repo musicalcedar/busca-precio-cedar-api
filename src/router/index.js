b134e07905ce0ca0250c549a96a6951559a13770
amr89-dev <amr89.dev@gmail.com>
2025-05-28 10:00:53 -0500

    Actualizar URLs de hojas de cálculo en el enrutador API y modificar la fecha de obtención a 28-05-2025 para una recuperación de datos precisa y coherente

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 
 const DB_URL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSu-J0yRnQCsxrmRNpJ-1MHBzLBEj3fxuCe4zPT1LWLFvBdBDVHEXVLiIU7nShPbXdH-3H5IvjjBn97/pub?gid=1528508115&single=true&output=csv";
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRhDeHzgwI2_1GQb0Lrqt13D-8kpf2WBwnFEiLE-1-ebDIGM_-AXJPDQNPXYAlzdi3-UFKkwVCX57qA/pub?gid=1944205918&single=true&output=csv";
 
 const LIST_URL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSu-J0yRnQCsxrmRNpJ-1MHBzLBEj3fxuCe4zPT1LWLFvBdBDVHEXVLiIU7nShPbXdH-3H5IvjjBn97/pub?gid=1958277438&single=true&output=csv";
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRhDeHzgwI2_1GQb0Lrqt13D-8kpf2WBwnFEiLE-1-ebDIGM_-AXJPDQNPXYAlzdi3-UFKkwVCX57qA/pub?gid=996919573&single=true&output=csv";
 
 const apiRouter = (server) => {
   const router = express.Router();
   server.use("/api", router);
 
   router.get("/data", async (req, res) => {
-    const FECHA = "27-05-2025";
+    const FECHA = "28-05-2025";
     try {
       const response = await fetch(DB_URL);
       const resText = await response.text();
